dbeac62760f7c158c8e0e3c69b271e09382afa00
Javid <j-n_01@hotmail.com>
2017-08-27 18:15:48 -0400

    added jquery + changes

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,1 +1,21 @@
-//Website Created by Javid Niftaliyev - April - 2017
+/**
+ * FileName: app.js
+ * Author: Javid Niftaliyev
+ * Description: This file is the main javascript file for the webpage
+ */
+
+(function () {
+    "use strict";
+
+// DOC READY STATEMENT IS REQUIRED FOR FUNCTION THAT ARE GOING TO BE HAPPENING TO THE DOCUMENT
+//Doc is leaded before we act
+    $(document).ready(function(){
+        //Basic Selectors
+        //$('*').css('border','4px solid red');
+
+        //Basic Animation
+       // $('.destinations img').animate({height: '200px'}, 300);
+
+    });
+
+})();
